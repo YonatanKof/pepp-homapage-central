cb94d0c0bda04179f511ca60a4c76b8402fe8e70
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2021-10-05 19:25:48 +0300

    add eyewear demo page and routs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,15 @@
 <template>
-	<router-link to="/">Home</router-link> | 
-	<router-link to="/about">About</router-link>
+	<div class="main-links">
+		<router-link to="/">Home</router-link>
+		<router-link to="/about">About</router-link>
+		<router-link to="/eyewear-demo">Eyewear Demo</router-link>
+	</div>
 </template>
+
+<style lang="scss">
+.main-links {
+	display: flex;
+	flex-direction: row;
+	gap: 1em;
+}
+</style>
---
+++ src/pages/EyewearDemo.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from "../components/Header.vue";
+import Slideshow from "../components/Slideshow.vue";
+import Promos from "../components/Promos.vue";
+import Portfolio from "../components/Portfolio.vue";
+import Footer from "../components/Footer.vue";
+</script>
+
+<template>
+	<body>
+		<Header />
+		<main class="wrapper content">
+			<Slideshow />
+			<Promos />
+			<Portfolio />
+			<Footer />
+		</main>
+	</body>
+</template>
