d952a12a8dbe1e10160f87cb1a5feeabc5b96cf2
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2021-10-05 19:24:58 +0300

    add button and SVGs

---
+++ src/components/ButtonIcon.vue
@@ -0,0 +1,19 @@
+<template>
+	<button class="button-weak-invert button-icon">
+		<IconSystem>
+			<slot />
+		</IconSystem>
+	</button>
+</template>
+
+<script>
+import IconSystem from "./IconSystem.vue";
+import IconMenu from "./icons/IconMenu.vue"
+export default {
+	name: "Button",
+	components : {
+		IconSystem,
+		IconMenu
+	}
+};
+</script>
---
+++ src/components/IconSystem.vue
@@ -0,0 +1,48 @@
+<template>
+	<svg
+		version="1.1"
+		viewBox="0 0 24 24"
+		aria-labelledby="title"
+		role="presentation"
+		:width="width"
+		:height="height"
+		:fill="iconColor"
+	>
+		<slot />
+		<rect id="bounding-box" fill="none" width="100%" height="100%"/>
+	</svg>
+</template>
+
+<script>
+export default {
+	props: {
+		width: {
+			type: [Number, String],
+			default: "1.5em",
+		},
+		height: {
+			type: [Number, String],
+			default: "1.5em",
+		},
+		iconColor: {
+			type: String,
+			default: "red",
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+svg {
+	transition: fill 0.35s ease-out;
+	&:hover {
+		fill: palevioletred;
+	}
+}
+p svg {
+	width: 1.2em;
+	height: 1.2em;
+	vertical-align: top;
+	margin-top: calc(1rem / -4);
+}
+</style>
