b3f2ccc3ae2d169952b818c92aeb35b0009fe7a6
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2021-10-10 17:37:44 +0300

    remove about page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
-import About from '../pages/About.vue'
+// import About from '../pages/About.vue'
 import EyewearDemo from '../pages/EyewearDemo.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
-    { path: '/about', name: 'About', component: About },
+    // { path: '/about', name: 'About', component: About },
     { path: '/eyewear-demo', name: 'Eyewear Demo', component: EyewearDemo }
 ]
 
