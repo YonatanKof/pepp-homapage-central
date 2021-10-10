277a5bfeeadafde16bbacb6df705df15f3d916df
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2021-10-10 17:39:16 +0300

    enrich home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>
-        <h1 class="title-4-lg">Home</h1>
-    </div>
+	<section>
+		<img src="../assets/pepperi-logo-full.svg" alt="">
+		<h1 class="title-4-lg">Pepperi's Homepage Central</h1>
+        <p>Click the above links to see our designs in action</p>
+	</section>
 </template>
+
+<style lang="scss" scoped>
+	section {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+		padding: 2rem 1rem;
+		height: 100vh;
+		background-color: #F7F7F7;
+		img {
+			max-width: 24rem;
+		}
+	}
+	
+</style>
