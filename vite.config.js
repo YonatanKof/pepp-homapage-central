aa1062cc85a619ad92e79e8f4e682cbc595054ce
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2021-10-11 17:10:59 +0300

    move scss file to root styles folder

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "./src/styles/abstracts" as *;`
+        additionalData: `@use "./src/styles" as *;`
       },
     },
   },
