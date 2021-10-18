0ef098d30eea1aece6efe7e47b5899c350e582e6
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2021-10-18 18:01:02 +0300

    add promo card

---
+++ src/components/demo-eyewear/PromoCard.vue
@@ -0,0 +1,48 @@
+<template>
+	<a class="item" :onclick="buttonOnClick" :style="`background-image: url(` + cardImageURL + `)`">
+		<div class="content">
+			<h3 class="title">{{ cardTitle }}</h3>
+		</div>
+	</a>
+</template>
+
+<script>
+export default {
+	name: "PromoCard",
+	props: ["buttonOnClick", "cardImageURL", "cardTitle"],
+};
+</script>
+<style lang="scss" scoped>
+.item {
+    aspect-ratio: 3/4;
+	@media screen and (max-width: $screen-sm) {
+        aspect-ratio: 2/1;
+	}
+	border-radius: $border-radius;
+	overflow: hidden;
+	background-size: cover;
+	@include clickable-image-base;
+}
+.title {
+	font-weight: $font-weight--bold;
+	font-size: $font-size-6-Xlarge;
+	color: $color-system-invert-1-full;
+	text-transform: capitalize;
+	@media screen and (max-width: $screen-md) {
+		font-size: $font-size-5-large;
+	}
+	@media screen and (max-width: $screen-sm) {
+		font-size: $font-size-6-Xlarge;
+	}
+}
+.content {
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	@include linear-gradient(180deg, black, 0.8, 50%);
+	padding: 1rem;
+	display: flex;
+}
+</style>
