c143035f44211cbc36d24b4457c308ba3623d72d
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2021-11-10 12:24:07 +0200

    add grid to App

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,9 @@ import HomepageCentral from "./components/HomepageCentral.vue";
 <style>
 @import "./assets/fonts/Montserrat/montserrat.css";
 @import "./assets/fonts/Inter/inter.css";
+#app {
+	height: 100vh;
+	display: grid;
+	grid-template-rows: auto 1fr;
+}
 </style>
