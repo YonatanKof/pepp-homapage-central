318af793d1b84f7cd2f70800e273ec2208f5cc11
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2021-12-29 19:59:05 +0200

    better animation and screen height responsive

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -16,18 +16,20 @@ import Logo from "../components/brand/BrandLogoFull.vue";
 			<h1 class="title-3xl color-system text-center">Homepage Central</h1>
 		</div>
 		<div class="nice-bg">
-			<SVGBase>
-				<SkinyLeaf :svgFill="'var(--color-primary-super-light)'" :svgOpacity="'1'" class="leaf leaf-skiny-alt" />
-			</SVGBase>
-			<SVGBase>
-				<RoundLeaf :svgFill="'var(--color-primary-extra-light)'" :svgOpacity="'1'" class="leaf leaf-round" />
-			</SVGBase>
-			<SVGBase>
-				<SkinyLeaf :svgFill="'var(--color-primary-light)'" :svgOpacity="'1'" class="leaf leaf-skiny" />
-			</SVGBase>
-			<SVGBase>
-				<FullLeaf :svgFill="'var(--color-primary-extra-light)'" :svgOpacity="'1'" class="leaf leaf-full" />
-			</SVGBase>
+			<div>
+				<SVGBase>
+					<SkinyLeaf :svgFill="'var(--color-primary-super-light)'" :svgOpacity="'1'" class="leaf leaf-skiny-alt" />
+				</SVGBase>
+				<SVGBase>
+					<RoundLeaf :svgFill="'var(--color-primary-extra-light)'" :svgOpacity="'1'" class="leaf leaf-round" />
+				</SVGBase>
+				<SVGBase>
+					<SkinyLeaf :svgFill="'var(--color-primary-light)'" :svgOpacity="'1'" class="leaf leaf-skiny" />
+				</SVGBase>
+				<SVGBase>
+					<FullLeaf :svgFill="'var(--color-primary-extra-light)'" :svgOpacity="'1'" class="leaf leaf-full" />
+				</SVGBase>
+			</div>
 		</div>
 	</section>
 </template>
@@ -39,7 +41,7 @@ section {
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	@media screen and (max-width: $screen-sm) {
+	@media screen and (max-width: $screen-sm), screen and (max-height: 500px) {
 		align-items: flex-start;
 	}
 	.intro-text {
@@ -66,12 +68,12 @@ section {
 			width: 32rem;
 			height: 100%;
 		}
-		@media screen and (max-width: $screen-xl) {
+		@media screen and (max-width: $screen-xl), screen and (max-height: 750px) {
 			svg {
 				width: 24rem;
 			}
 		}
-		@media screen and (max-width: $screen-sm) {
+		@media screen and (max-width: $screen-sm), screen and (max-height: 500px) {
 			margin-top: 2rem;
 			svg {
 				width: 20rem;
@@ -84,124 +86,164 @@ section {
 	position: fixed;
 	inset: 0px;
 	background-color: $color-system-4-weak;
+	background-image: radial-gradient(var(--color-system-00) 20%, var(--color-primary-extra-light));
+	div {
+		background-image: url("/map.svg");
+		height: 100%;
+		opacity: 0.9;
+	}
 }
-@keyframes leaf-anim {
+@keyframes leaf-anim-1 {
 	from {
 		translate: 0% 0%;
 	}
 	to {
 		translate: 6% 3%;
+		rotate: -8deg;
+	}
+}
+@keyframes leaf-anim-2 {
+	from {
+		translate: 0% 0%;
+	}
+	50% {
+		translate: -6% -3%;
+		rotate: 8deg;
+	}
+	to {
+		translate: 0% 0%;
 	}
 }
 .leaf {
 	position: absolute;
 	inset: unset;
 	transition: inset ease-out 4s, transform ease-out 2s;
-	animation: leaf-anim 12000ms cubic-bezier(0.35, 0, 0.75, 1) alternate infinite;
 }
 .leaf-full {
 	right: -1rem;
 	bottom: -18rem;
 	transform: rotateZ(-50deg) scale(1);
-	@media screen and (max-width: $screen-xl) {
+	animation: leaf-anim-1 24000ms cubic-bezier(0.35, 0, 0.75, 1) alternate infinite;
+	@media screen and (max-width: $screen-xl), screen and (max-height: 750px) {
 		transform: rotateZ(-40deg) scale(0.9);
 	}
-	@media screen and (max-width: $screen-lg) {
+	@media screen and (max-width: $screen-lg), screen and (max-height: 700px) {
 		right: -2rem;
 		bottom: -19rem;
 		transform: rotateZ(-30deg) scale(0.8);
 	}
-	@media screen and (max-width: $screen-md) {
+	@media screen and (max-width: $screen-md), screen and (max-height: 650px) {
 		right: -5rem;
 		bottom: -20rem;
 		transform: rotateZ(-25deg) scale(0.7);
 	}
-	@media screen and (max-width: $screen-sm) {
+	@media screen and (max-width: $screen-sm), screen and (max-height: 500px) {
 		right: -9rem;
 		bottom: -23rem;
 		transform: rotateZ(-20deg) scale(0.6);
+	}
+	@media screen and (max-width: $screen-xs) {
+		right: -9rem;
+		bottom: -26rem;
+		transform: rotate(-20deg) scale(0.45);
 	}
 }
 .leaf-round {
 	left: -6rem;
 	top: -2rem;
-	animation-delay: 2s;
+	animation: leaf-anim-1 16000ms ease-in alternate infinite;
 	transform: rotateZ(-20deg) scale(1.3) skew(5deg, 5deg);
 	@media screen and (max-width: $screen-3xl) {
 		transform: rotateZ(-25deg) scale(1.2) skew(5deg, 5deg);
 	}
-	@media screen and (max-width: $screen-2xl) {
+	@media screen and (max-width: $screen-2xl), screen and (max-height: 800px) {
 		transform: rotateZ(-25deg) scale(1.1) skew(5deg, 5deg);
 	}
-	@media screen and (max-width: $screen-xl) {
+	@media screen and (max-width: $screen-xl), screen and (max-height: 750px) {
 		transform: rotateZ(-25deg) scale(1) skew(5deg, 5deg);
 	}
-	@media screen and (max-width: $screen-lg) {
+	@media screen and (max-width: $screen-lg), screen and (max-height: 700px) {
 		left: -7rem;
 		top: -2.5rem;
 		transform: rotateZ(-20deg) scale(0.9) skew(2deg, 2deg);
 	}
-	@media screen and (max-width: $screen-md) {
+	@media screen and (max-width: $screen-md), screen and (max-height: 650px) {
 		left: -8rem;
 		top: -3rem;
 		transform: rotateZ(-15deg) scale(0.8) skew(5deg, 5deg);
 	}
-	@media screen and (max-width: $screen-sm) {
+	@media screen and (max-width: $screen-sm), screen and (max-height: 500px) {
 		top: -7rem;
 		left: -13rem;
 		transform: rotateZ(-10deg) scale(0.6) skew(1deg, 1deg);
+	}
+	@media screen and (max-width: $screen-xs) {
+		top: -8rem;
+		left: -15rem;
+		transform: rotateZ(-5deg) scale(0.5) skew(1deg, 1deg);
 	}
 }
 .leaf-skiny {
 	left: -6rem;
 	bottom: -10rem;
-	animation-delay: 4s;
+	animation-delay: 2s;
+	animation: leaf-anim-2 16000ms ease-in-out alternate infinite;
 	transform: rotateZ(55deg) scale(1.1);
-	@media screen and (max-width: $screen-2xl) {
+	@media screen and (max-width: $screen-2xl), screen and (max-height: 800px) {
 		transform: rotateZ(50deg) scale(1);
 	}
-	@media screen and (max-width: $screen-xl) {
+	@media screen and (max-width: $screen-xl), screen and (max-height: 750px) {
 		transform: rotateZ(45deg) scale(0.9);
 	}
-	@media screen and (max-width: $screen-lg) {
+	@media screen and (max-width: $screen-lg), screen and (max-height: 700px) {
 		left: -11rem;
 		transform: rotateZ(40deg) scale(0.8);
 	}
-	@media screen and (max-width: $screen-md) {
+	@media screen and (max-width: $screen-md), screen and (max-height: 650px) {
 		left: -12rem;
 		transform: rotateZ(35deg) scale(0.75);
 	}
-	@media screen and (max-width: $screen-sm) {
+	@media screen and (max-width: $screen-sm), screen and (max-height: 500px) {
 		left: -11rem;
 		bottom: -16rem;
 		transform: rotateZ(30deg) scale(0.7);
+	}
+	@media screen and (max-width: $screen-xs) {
+		left: -18rem;
+		bottom: -20;
+		transform: rotate(30deg) scale(0.5);
 	}
 }
 .leaf-skiny-alt {
 	right: -6rem;
 	top: -9rem;
 	transform: rotateZ(-110deg) skew(10deg, 5deg) scale(1.1);
-	animation-delay: 6s;
-	@media screen and (max-width: $screen-2xl) {
+	animation: leaf-anim-2 24000ms cubic-bezier(0.35, 0, 0.75, 1) alternate infinite;
+	@media screen and (max-width: $screen-2xl), screen and (max-height: 800px) {
 		transform: rotateZ(-120deg) skew(10deg, 5deg) scale(1);
 	}
-	@media screen and (max-width: $screen-xl) {
+	@media screen and (max-width: $screen-xl), screen and (max-height: 750px) {
 		transform: rotateZ(-130deg) skew(10deg, 5deg) scale(0.9);
 	}
-	@media screen and (max-width: $screen-lg) {
+	@media screen and (max-width: $screen-lg), screen and (max-height: 700px) {
 		right: -7rem;
 		top: -10rem;
 		transform: rotateZ(-140deg) skew(10deg, 5deg) scale(0.8);
 	}
-	@media screen and (max-width: $screen-md) {
+	@media screen and (max-width: $screen-md), screen and (max-height: 650px) {
 		right: -10rem;
 		top: -11rem;
 		transform: rotateZ(-150deg) skew(10deg, 5deg) scale(0.7);
 	}
-	@media screen and (max-width: $screen-sm) {
+	@media screen and (max-width: $screen-sm), screen and (max-height: 500px) {
 		right: -12rem;
 		top: -18rem;
 		transform: rotateZ(-160deg) skew(10deg, 5deg) scale(0.6);
+	}
+	@media screen and (max-width: $screen-xs) {
+		right: -18rem;
+		top: -20rem;
+		transform: rotate(-150deg) skew(5deg, 3deg) scale(0.4);
 	}
 }
 </style>
